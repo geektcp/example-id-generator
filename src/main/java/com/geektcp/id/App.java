ace43afd6b215882f7c06ba40e97c9b2b253f4c0
geektcp <geektcp@163.com>
2023-01-29 21:22:28 +0800

    update example code

--- src/main/java/com/geektcp/id/App.java
+++ src/main/java/com/geektcp/id/App.java
@@ -1,30 +1,54 @@
 package com.geektcp.id;
 
-import com.geektcp.common.core.util.IdUtils;
+import com.geektcp.common.core.generator.IdGenerator;
 
 public class App {
     public static void main(String[] args) {
+        base();
+        singleGen();
         multiThread();
     }
 
-    private static void singleGen(){
-        for (int i =0; i<1000;i++){
-            System.out.println(IdUtils.getId());
+
+    private static void base() {
+        long a = 0;
+        String c = null;
+
+        a = IdGenerator.getId();
+        System.out.println(a);
+        a = IdGenerator.getId(1, 3);
+
+        System.out.println(a);
+        IdGenerator.setSplit("/");
+        c = IdGenerator.getId("PRE");
+
+        System.out.println(c);
+        IdGenerator.setInstance(10, 0);
+
+        a = IdGenerator.getId();
+
+        System.out.println(a);
+    }
+
+
+    private static void singleGen() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(IdGenerator.getId());
         }
     }
 
-    private static void multiThread(){
-        new Thread(()->{
-            for (int i =0; i<1000;i++){
-                System.out.println(IdUtils.getId("A"));
+    private static void multiThread() {
+        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println(IdGenerator.getId("A"));
             }
-        },"A").start();
+        }, "A").start();
 
-        new Thread(()->{
-            for (int i =0; i<1000;i++){
-                System.out.println(IdUtils.getId("B"));
+        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println(IdGenerator.getId("B"));
             }
-        },"A").start();
+        }, "A").start();
 
     }
 
